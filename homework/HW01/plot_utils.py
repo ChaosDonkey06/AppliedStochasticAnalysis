09f107f8fdd39f3d307e07a75cd5004ce350298d
Jaime Cascante <je.cascante10@uniandes.edu.co>
2024-02-08 16:41:59 -0500

    update figure'

--- homework/HW01/plot_utils.py
+++ homework/HW01/plot_utils.py
@@ -15,7 +15,7 @@ MEDIUM_SIZE = 15
 BIGGER_SIZE = 18
 
 plt.rc('font', size=SMALL_SIZE, family='sans-serif', serif='Arial')          # controls default text sizes
-plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+plt.rc('axes', titlesize=BIGGER_SIZE)    # fontsize of the axes title
 plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
 plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels"
 plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
