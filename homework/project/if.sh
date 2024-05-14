e62b62dd09e6ece3604a01f8a3787d0f73a509a3
Jaime Cascante Vega <je.cascante10@uniandes.edu.co>
2024-05-14 15:53:39 -0400

    update if.sh

--- homework/project/if.sh
+++ homework/project/if.sh
@@ -3,17 +3,18 @@
 #SBATCH --job-name=ASA_project
 #SBATCH --output=out/if-pf%A_%a.out
 #SBATCH --error=error/if-pf%A_%a.err
-#SBATCH --partition=broadwl
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=10
-#SBATCH --mem-per-cpu=1000
-#SBATCH --cpus-per-task=1
 #SBATCH --time=0:45:00
 #SBATCH --mail-type=END
 #SBATCH --mail-user=jc12343@nyu.edu
-#SBATCH --output=/results/slurm_%j.txt
+#SBATCH --partition=cs
 #SBATCH --array=0-99
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=10
+#SBATCH --mem-per-cpu=10GB
+#SBATCH --cpus-per-task=1
+
 #SBATCH --chdir=/scratch/jc12343/AppliedStochasticAnalysis/homework/project
+
 
 module purge
 module load python/intel/3.8.6
